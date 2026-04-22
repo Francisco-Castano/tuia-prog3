7f8adadb5e4351d20db73ed49582b2aa50936af9
Francisco-Castano <franciscohugocastano@gmail.com>
2026-04-22 12:33:42 -0300

    Cargado de astar y dfs

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -5,6 +5,14 @@ from ..models.node import Node
 
 
 class AStarSearch:
+
+    @staticmethod
+    def heuristica(grid_end: tuple[int,int], grid_pos: tuple[int,int]):
+        """Retorna el valor heuristico usando la distancia Manhattan."""
+        x1, x2 = grid_pos
+        y1, y2 = grid_end
+        return abs(x1 - y1) + abs(x2 - y2)
+
     @staticmethod
     def search(grid: Grid) -> Solution:
         """Find path between two points in a grid using A* Search
@@ -22,8 +30,39 @@ class AStarSearch:
         reached = {}
         reached[root.state] = root.cost
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        #Inicializamos la front con el costo heuristico
+        frontera = PriorityQueueFrontier()
+        frontera.add(node=root, priority=root.cost + AStarSearch.heuristica(grid.end, root.state))
 
+        #Si la front no esta vacia, expandimos
+        while not frontera.is_empty():
+
+            nodo_desc = frontera.pop()
+
+            #Hacemos el test objetivo del nodo desencolado
+            if grid.objective_test(nodo_desc.state):
+                return Solution(node=nodo_desc,
+                                reached=reached)
+
+            #Si no es una solucion, evaluamos mas acciones
+            for accion in grid.actions(nodo_desc.state):
+                estado_alcanzado = grid.result(pos=nodo_desc.state,
+                                               action=accion)
+
+                costo_accion = nodo_desc.cost + grid.individual_cost(pos=nodo_desc.state,
+                                                                     action=accion)
+
+                #Verificacion de si no alcanzamos el estado o si llegamos al mismo estado con un costo menor
+                if estado_alcanzado not in reached or costo_accion < reached[estado_alcanzado]:
+                    nodo_hijo = Node("",
+                                     state=estado_alcanzado,
+                                     cost=costo_accion,
+                                     parent=nodo_desc,
+                                     action=accion)
+
+                    #Agregamos el estado alcanzado y el nodo a la frontera con su costo heuristico
+                    reached[estado_alcanzado] = costo_accion
+                    frontera.add(node=nodo_hijo,
+                                 priority=nodo_hijo.cost + AStarSearch.heuristica(grid_end=grid.end,
+                                                                                  grid_pos=nodo_hijo.state))
         return NoSolution(reached)
--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -18,11 +18,40 @@ class DepthFirstSearch:
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
 
+        # Initialize frontier with the root node
+        # TODO Complete the rest!!
+       
+        root = Node("", state=grid.initial, cost=0, parent=None, action=None)
+
         # Initialize expanded with the empty dictionary
         expanded = dict()
 
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontera = StackFrontier()
+        frontera.add(node=root)
 
-        return NoSolution(expanded)
+        #Comprobamos que la front no este vacia
+        while not frontera.is_empty():
+
+            #Desapilamos el nodo
+            nodo_desp = frontera.remove()
+
+            for accion in grid.actions(pos=nodo_desp.state):
+                estado_alcanzado = grid.result(pos=nodo_desp.state,
+                                               action=accion)
+
+                if estado_alcanzado not in expanded:
+                    nodo_hijo = Node("",
+                                     estado_alcanzado,
+                                     nodo_desp.cost + grid.individual_cost(nodo_desp.state, accion),
+                                     parent=nodo_desp,
+                                     action=accion)
+
+                    if grid.objective_test(pos=nodo_hijo.state):
+                        return Solution(node=nodo_hijo,
+                                        reached=expanded)
+
+                    expanded[nodo_hijo.state] = True
+                    frontera.add(node=nodo_hijo)
+
+        return NoSolution(reached=expanded)       
