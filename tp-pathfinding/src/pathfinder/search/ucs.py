4c22b40c91b60e5e97c498998f99de1a21dc4a6b
Francisco-Castano <franciscohugocastano@gmail.com>
2026-04-23 13:23:53 -0300

    Agrgo el ucs

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -25,5 +25,33 @@ class UniformCostSearch:
         # Initialize frontier with the root node
         # TODO Complete the rest!!
         # ...
+        #Inicializamos la frontera y encolamos la raiz
+        frontera = PriorityQueueFrontier()
+        frontera.add(node=root, priority=root.cost)
+
+        #Si la cola no esta vacia, expandimos.
+        while not frontera.is_empty():
+
+            nodo_desc = frontera.pop()
+
+            if grid.objective_test(nodo_desc.state):
+                return Solution(node=nodo_desc,
+                                reached=reached)
+
+            for accion in grid.actions(nodo_desc.state):
+                estado_alcanzado = grid.result(pos=nodo_desc.state,
+                                               action=accion)
+                costo_accion = nodo_desc.cost + grid.individual_cost(pos=nodo_desc.state,
+                                                                     action=accion)
+
+                if estado_alcanzado not in reached or costo_accion < reached[estado_alcanzado]:
+                    nodo_hijo = Node("", state=estado_alcanzado,
+                                     cost=costo_accion,
+                                     parent=nodo_desc,
+                                     action=accion)
+
+                    reached[estado_alcanzado] = costo_accion
+                    frontera.add(node=nodo_hijo,
+                                 priority=costo_accion)
 
         return NoSolution(reached)
